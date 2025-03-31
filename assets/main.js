a29f64008256190e35292dd097fc4e5a213b55d1
Paul-16098 <pl816098@gmail.com>
2025-03-31 17:58:28 +0800

    新增主題顏色及頁面結構調整，新增GitHub統計擴展，更新快取時間及參數設定

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+"use strict";
+switch (pageData.slug) {
+    case "stats": {
+        setTimeout(() => location.reload(), 1 * 60 * 1000);
+        break;
+    }
+}
+// document.onchange = function () {};
+//# sourceMappingURL=main.js.map
