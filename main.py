610e10de958872fe4285d4eb5ba145a5459c6eb0
Paul-16098 <pl816098@gmail.com>
2025-04-04 15:09:37 +0800

    更新glance.yml配置，新增搜尋功能及相關快捷鍵，調整main.py以支持命令行參數執行

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ import dotenv
 import os
 
 
-dotenv.load_dotenv()
+def main(
+    # glance args
+    args: tuple[str, ...] = (
+        # print true cofg
+        "config:print>glance.t.yml",
+        # diagnose web
+        # "diagnose",
+        # test env
+        "config:validate",
+        # run
+        "",
+    ),
+):
+    """run and test glance
 
-os.system("glance diagnose")
-os.system("glance config:validate")
-# os.system("glance config:print")
-os.system("glance")
+    :param args: glance args
+    :type args: tuple[str, ...]"""
+    dotenv.load_dotenv()
+
+    for arg in args:
+        try:
+            os.system(f"{os.path.join(os.path.dirname(__file__), 'glance')} {arg}")
+        except KeyboardInterrupt:
+            if arg == "":
+                # run
+                print("\r---- reload ----")
+                main(("",))
+            else:
+                # not run
+                print("\r---- abort ----")
+                return
+
+
+if __name__ == "__main__":
+    main()
